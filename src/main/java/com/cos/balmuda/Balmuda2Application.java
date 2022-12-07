800d79e2e59c95539825a0c213c68d4ca11ef61f
rladhdud2 <119910066+rladhdud2@users.noreply.github.com>
2022-12-07 14:52:43 +0900

    test

---
+++ src/main/java/com/cos/balmuda/Balmuda2Application.java
@@ -0,0 +1,13 @@
+package com.cos.balmuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Balmuda2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Balmuda2Application.class, args);
+	}
+
+}
